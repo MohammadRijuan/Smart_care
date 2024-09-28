634b4b6bbee2cf351b435b418523a8379e49ca3f
Rijuan <mohammadrijuan2529@gmail.com>
2024-09-28 21:09:10 +0600

    hospital api create with rest framework

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-xw92_ix)^5pulb6z_g#495599*2rhwlw(fa!wg4zuefz#tu_=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
